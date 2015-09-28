60640fd6ec1f2558c428ce7a58a30a2df40db4ce
Johnny Rodriguez <johnnydrodriguez@gmail.com>
2015-09-27 22:34:03 -0400

    Task 01 File for getting current date.

---
+++ task_01.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This is the module which assigns the current date. """
+
+
+import datetime
+
+
+CURDATE = None
+
+def get_current_date():
+    """This functions gets the current date..
+
+    Args:
+        None
+
+    Returns:
+        Date Object: Returns the current date.
+
+    Examples:
+
+        >>> get_current_date()
+        datetime.date(2015, 9, 27)
+    """
+    return datetime.date.today()
+
+if __name__ == '__main__':
+    CURDATE = get_current_date()
+    print CURDATE
